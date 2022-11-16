a0e50752ed3daffb2527498385b7980c4df9b5af
bkhorvat <bkhorvat.php@gmail.com>
2022-11-16 13:50:42 +0100

    instanceof

--- 1/index.php
+++ 1/index.php
@@ -18,8 +18,8 @@ $product1 = new ShoppingCard();
 $product2 = new ShoppingCard();
 $product3 = new ShoppingCard();
 
-var_dump($product1);
-var_dump($product2);
+var_dump($product1 instanceof ShoppingCard);
+var_dump($product2 instanceof Card);
 var_dump($product3);
 
 ?>
