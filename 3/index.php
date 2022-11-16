e496ddd8254b1c0cdc3bd80fbc52eb9a3a18eba6
bkhorvat <bkhorvat.php@gmail.com>
2022-11-16 14:16:00 +0100

    how work public function

---
+++ 3/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+class User
+{
+    public $name = "name";
+    private $password = "password";
+    public $email = "email";
+    protected $city = "city";
+
+    public function Hello()
+    {
+        echo "Hello {$this->name}!";
+    }
+}
+
+$admin = new User;
+$admin->name = "Bohdan";
+$admin->Hello();
+
+?>
+
+</body>
+</html>
