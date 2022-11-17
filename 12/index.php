0459a9d41b6dbe61305b21e5abd42f58985a55b4
bkhorvat <bkhorvat.php@gmail.com>
2022-11-17 10:28:40 +0100

    traits

--- 12/index.php
+++ 12/index.php
@@ -1,3 +1,36 @@
 <?php
 
+class Base
+{
+    public function sayHello()
+    {
+        echo "Hello ";
+    }
+}
+trait sayName
+{
+    public function sayName()
+    {
+        echo " Bogdan.";
+    }
+}
+
+trait sayWorld
+{
+    public function sayHello()
+    {
+        parent::sayHello();
+        echo "world";
+    }
+}
+
+class MyHelloWorld extends Base
+{
+    use sayWorld, sayName; //use Hello, Hello; //error
+}
+
+$obj = new MyHelloWorld;
+$obj->sayHello();
+$obj->sayName();
+
 ?>
